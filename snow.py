a39d280552c4bf995e0f6de62845fdfa73d2fca4
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-07-19 11:35:05 +0800

    使用fire控制参数

--- snow.py
+++ snow.py
@@ -5,52 +5,55 @@ import inspect
 import tempfile
 import platform
 
+import fire
+
 import 召唤
 
-if platform.system()=='Windows':
+
+if platform.system() == 'Windows':
     import 位置控制
     位置控制.自动调整()
 
 
-虚无 = True
-独立 = False
+def nya(file=None, 虚无=True, 独立=True):
+    try:
+        if file is None:
+            os.system('pmd')
+            sys.exit()
 
-def nya():
-    if len(sys.argv) == 1:
-        os.system('pmd')
-        sys.exit()
+        全名 = file
+        全名.replace('/', '\\')
+
+        路径, 文件名 = os.path.split(全名)
+        主名, 扩展名 = os.path.splitext(文件名)
+        扩展名 = 扩展名[1:]
+        logging.info(f'{路径=}, {文件名=}, {主名=}, {扩展名=}')
+
+        if 独立:
+            print('------------------------------------------------------------')
+            os.system(f'title {文件名} - snow')
+
+        os.chdir(路径)
+
+        exe = f'{主名}.exe'
+        if 虚无:
+            exe = f'{tempfile.gettempdir()}\\the_program_of_Rimo.exe'
+            路径 = tempfile.gettempdir()
+
+        if 扩展名 in 召唤.__dict__:
+            f = 召唤.__dict__[扩展名]
+            需要的参数 = inspect.getfullargspec(f).args
+            t = locals()
+            填充 = {i: t[i] for i in 需要的参数}
+            f(**填充)
+        else:
+            print(f'「{文件名}」这什么后缀反正我没做')
+    except Exception as e:
+        logging.exception(e)
+
     if 独立:
-        print('------------------------------------------------------------')
-        os.system(f'title {文件名} - snow')
- 
-    全名 = sys.argv[1]
-    全名.replace('/', '\\')
-
-    路径, 文件名 = os.path.split(全名)
-    主名, 扩展名  = os.path.splitext(文件名)
-    扩展名 = 扩展名[1:]
-    logging.info(f'{路径=}, {文件名=}, {主名=}, {扩展名=}')
-    
-    exe = f'{主名}.exe'
-    if 虚无:
-        exe = f'{tempfile.gettempdir()}\\the_program_of_Rimo.exe'
-        路径 = tempfile.gettempdir()
-
-    if 扩展名 in 召唤.__dict__:
-        f = 召唤.__dict__[扩展名]
-        需要的参数 = inspect.getfullargspec(f).args
-        t = locals()
-        填充 = {i:t[i] for i in 需要的参数}
-        f(**填充)
-    else:
-        print(f'「{文件名}」这什么后缀反正我没做')
+        print()
+        os.system('pause')
 
 
-try:
-    nya()
-except Exception as e:
-    logging.exception(e)
-
-if 独立:
-    print()
-    os.system('pause')
+fire.Fire(nya)
